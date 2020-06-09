a47f8e419778e5d4fa8cb8130a4722e9c7fa024e
Daniel Rocha <rochadaniel15@gmail.com>
2020-06-09 16:38:47 -0300

    Refatoração

--- lazy.sh
+++ lazy.sh
@@ -10,61 +10,50 @@ LC_TIME=en_US.utf8
     repository="green"
     path=$(pwd)
     date=$(date)
-    check=$(false)
+    main_file="file.txt"
+    
+    from=$1
+    to=$2
 
-datediff() {
-    # >> $ datediff '1 Nov' '1 Aug'
-    # >> 91 days
+datediff() 
+{
     
     date_from=$(date -d "$1" +%s)
     date_to=$(date -d "$2" +%s)
     
-    date_diff=$(( (date_from - date_to) / 86400 ))
+    a_day_in_seconds=86400 
 
-    if [ $date_diff -lt  0 ];
-    then
-        date_diff=$(( date_diff * -1 ))
-    fi
+    date_diff=$(( (date_to - date_from) / a_day_in_seconds))
 
     echo $date_diff
 }
 
-    from=$1
-    to=$2
 
-    date_between=$(datediff "$from" "$to")
-
-
-git_add()
+add_commit()
 {
-    
-    cd "$path/$repository/"         \
-    && touch -d "$from" .gitignore  \
-    && date >> file.txt             \
-    && git add .                    \
-    && git commit -m "  
-        FAKE - $1    
- 
-        Adding a fake commit git"   \
-        > /dev/null                 \
-    && git commit --amend --no-edit --date "$1" > /dev/null
+    commit_date=$1
+                        
+   cd "$path/$repository/"                  \
+    && date >> "$main_file"                 \
+    && git add .                            \
+    && git commit -m "FAKE - $commit_date"  \
+       --date "$commit_date" > /dev/null
+     
 }
 
 
-create_fake_repository()
+create_repository()
 {
-    #   CRIAR PASTA PARA O REPOSITORIO FAKE
+   
     if ! [ -d "$path/$repository" ];
     then
-        mkdir $repository
-    fi
+        
+        mkdir $repository 
 
-
-    #   INICIAR REPOSITORIO
-    if ! [ -d "$path/$repository/.git/" ];
-    then
-        echo ">> Repositorio Criado: '$repository' \r"
-        git init "$path/$repository/" > /dev/null
+        cd "$repository/"           \
+        && touch -d "$1" .gitignore \
+        && git init > /dev/null
+    
     fi
 
 }
@@ -72,31 +61,57 @@ create_fake_repository()
 
 handle_input()
 {
-    _message="
-    Usage: ./fake.sh 'date' 'date' 'repository-name'\n
-    Try './fake --help or -h' for more information.
-    "
 
-    _help="
-    Usage: ./fake.sh <date> <date> 'repository-name'\n
-        \t--help | -h : display this help and exit\n
-        \n    
-        \tdate: '1 Nov 15' day, mouth, year \n
-        \n
-        \tExample: \n
-            \t\t$ ./fake.sh \"1 Nov 20\" \"20 Nov 19\" \"sleep-project\"
-   
-    "
+_message=$(cat << EOF
+Usage: ./lazy.sh 'date from' 'date to' 'repository-name'\n
+Try './lazy --help or -h' for more information.
+EOF
+)
 
-    if [ $1 = "--help" ] || [ $1 = "-h" ];
+_help=$(cat << EOF
+ Usage: ./lazy.sh <date from> <date to> <repository-name>
+    options    
+        --help\t | -h : display this help and exit
+        --global | -g : make available throughout the system
+        --remove | -r : remove from the system
+        --version
+        
+            date: '1 Nov 15' day, month, year
+        
+        Example:
+            $ ./lazy.sh "1 Nov 20" "20 Nov 19" "sleep-project"
+
+EOF
+)
+    
+    if [ "$1" = "--help" ] || [ "$1" = "-h" ];
     then
-        echo $_help
-        exit 1
+        echo "$_help"
+        exit 0
     fi
 
-    if [ -z $1 ] || [ -z $2 ];
+    if [ "$1" = "--global" ] || [ "$1" = "-g" ];
     then
-        echo $_message
+        echo "Adicionando"
+        exit 0
+    fi
+
+    if [ "$1" = "--remove" ] || [ "$1" = "-r" ];
+    then
+        echo "Removendo"
+        exit 0
+    fi
+
+    # Renomear repositorio
+    if ! [ -z $3 ];
+    then
+        repository="$3"
+    fi
+
+
+    if [ -z "$1" ] || [ -z "$2" ];
+    then
+        echo "$_message"
         exit 1
     fi
 
@@ -105,26 +120,36 @@ handle_input()
 
 fill_interval()
 {
-    interator=0
+
+    from="$1"
+    to="$2"
+
+    days=$(datediff "$from" "$to")
+    
+
+    if [ $days -le  0 ];
+    then
+        echo "Dates must be 'from': past 'to': future\n\tUse: ./lazy -h\n"
+        exit 1
+    fi
+
+
     echo ">> Processing ..."
-    while [ $interator -le $date_between ]
+    
+    interator=0
+    while [ $interator -le $days ]
     do
         interator=$(( interator + 1 ))
-        new_date=$(date -d "$from $interator days" +%a' '%b' '%d' '%H:%M:%S' '%Y' '%z)  
-        git_add "$new_date"
+        commit_date=$(date -d "$from $interator days" +%a' '%b' '%d' '%H:%M:%S' '%Y' '%z)  
+        add_commit "$commit_date"
     done
 
     echo ">> Finished"
-    echo ">> Fake repository: \n\t$path/$repository"
+    echo ">> Generated repository from: '$from' to: '$to':"
+    echo "   Report:\n\tDays:    $days\n\tCommits: $interator\n"
+    echo "   Path repository: $path/$repository\n"
 }
 
-handle_input $1 $2
-
-if ! [ -z $3 ];
-then
-    repository=$3
-fi
-
-create_fake_repository
-
-fill_interval
+handle_input "$1" "$2" "$3"
+create_repository "$from"
+fill_interval "$from" "$to"
